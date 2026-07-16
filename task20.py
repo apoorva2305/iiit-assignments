8fe57353aa71e75fb8ffd1a33a6e64507a9585f1
apoorva2305 <apoorva.s2006@gmail.com>
2026-07-16 15:40:05 +0530

    Add files via upload

---
+++ task20.py
@@ -0,0 +1,10 @@
+# minimum of 2 numbers
+# Take input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+minimum_num = min(num1, num2)
+
+
+print(f"The minimum number is: {minimum_num}")
+
