cb717990dee5006bb73138107b72161b6aa1198c
apoorva2305 <apoorva.s2006@gmail.com>
2026-07-20 16:12:51 +0530

    Add files via upload

---
+++ task30.py
@@ -0,0 +1,9 @@
+def sum_of_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+# Example usage:
+my_list = [10, 20, 30, 40]
+print(sum_of_list(my_list))  # Output: 100
